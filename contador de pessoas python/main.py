37aafe12854b17aaafcce556fa557f8b3cff98c6
Mauro Ramires <31906146+mauroramiressaracho@users.noreply.github.com>
2023-09-28 14:19:59 -0400

    update do código

--- contador de pessoas python/main.py
+++ contador de pessoas python/main.py
@@ -3,10 +3,12 @@
 #CAMPO GRANDE MS - ALUNOS
 # ALUNOS DO PROJETO - MAURO, CAIQUE, GABRIEL, GUSTAVO, YUNES, BELMANTE, WANDERLEY
 
-# Importando a biblioteca OpenCV, que é usada para processar vídeos e imagens.
+# Importando as bibliotecas
 import cv2
+import csv
+import datetime
 
-# Definindo uma função para calcular o centro de um retângulo.
+# Função para calcular o centro de um retângulo
 def center(x, y, w, h):
     x1 = int(w / 2)
     y1 = int(h / 2)
@@ -14,71 +16,78 @@ def center(x, y, w, h):
     cy = y + y1
     return cx, cy
 
-# Inicializando a captura de vídeo a partir de um arquivo chamado 'entrada_loja.mp4'.
+# Inicialização da captura de vídeo a partir de um arquivo
 cap = cv2.VideoCapture('entrada_loja.mp4')
 
-# Criando um objeto de subtração de fundo.
+# Inicialização do algoritmo de subtração de fundo
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
-# Uma lista vazia para rastrear as detecções.
+# Lista para armazenar detecções
 detects = []
 
-# Configurando uma linha na imagem para contar as pessoas.
+# Posição da linha de referência e deslocamento para a detecção
 posL = 150
 offset = 30
 
-# Coordenadas para desenhar uma linha horizontal.
+# Coordenadas para desenhar a linha de referência
 xy1 = (20, posL)
 xy2 = (300, posL)
 
-# Inicializando contadores para rastrear pessoas.
+# Inicialização de contadores
 total = 0
 up = 0
 down = 0
 
-# Loop principal que processa cada quadro do vídeo.
+# Abre um arquivo CSV para escrita e escreve o cabeçalho
+with open('registro_entradas.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Data e Hora'])
+
+# Loop principal do programa
 while 1:
-    ret, frame = cap.read()  # Lendo um quadro do vídeo.
+    ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convertendo o quadro para escala de cinza.
+    # Conversão do quadro para escala de cinza
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    fgmask = fgbg.apply(gray)  # Aplicando subtração de fundo para destacar objetos em movimento.
+    # Aplicação do algoritmo de subtração de fundo
+    fgmask = fgbg.apply(gray)
 
-    # Thresholding para criar uma imagem binária.
+    # Limiarização da imagem
     retval, th = cv2.threshold(fgmask, 200, 255, cv2.THRESH_BINARY)
 
-    # Aplicando operações de morfologia para melhorar a detecção.
+    # Criação de um kernel para operações de morfologia
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+
+    # Aplicação de operações morfológicas
     opening = cv2.morphologyEx(th, cv2.MORPH_OPEN, kernel, iterations=2)
     dilation = cv2.dilate(opening, kernel, iterations=8)
     closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel, iterations=8)
 
-    # Desenhando linhas e contando objetos.
+    # Desenha linhas de referência na imagem
     cv2.line(frame, xy1, xy2, (255, 0, 0), 3)
     cv2.line(frame, (xy1[0], posL - offset), (xy2[0], posL - offset), (255, 255, 0), 2)
     cv2.line(frame, (xy1[0], posL + offset), (xy2[0], posL + offset), (255, 255, 0), 2)
 
-    # Encontrando contornos na imagem.
+    # Encontra contornos na imagem
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     i = 0
-
-    # Loop para processar cada contorno encontrado.
     for cnt in contours:
         (x, y, w, h) = cv2.boundingRect(cnt)
         area = cv2.contourArea(cnt)
-
+        
         if int(area) > 3000:
             centro = center(x, y, w, h)
-
-            # Desenhando um retângulo e um círculo ao redor do objeto detectado.
+            
+            # Desenha informações sobre os objetos detectados
             cv2.putText(frame, str(i), (x + 5, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
             cv2.circle(frame, centro, 4, (0, 0, 255), -1)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-            # Verificando se o objeto está próximo da linha.
+            
+            # Armazena as detecções na lista 'detects'
             if len(detects) <= i:
                 detects.append([])
-
+                
             if centro[1] > posL - offset and centro[1] < posL + offset:
                 detects[i].append(centro)
             else:
@@ -95,7 +104,6 @@ while 1:
     else:
         for detect in detects:
             for (c, l) in enumerate(detect):
-                # Verificando se um objeto cruzou a linha.
                 if detect[c - 1][1] < posL and l[1] > posL:
                     detect.clear()
                     up += 1
@@ -108,23 +116,33 @@ while 1:
                     down += 1
                     total += 1
                     cv2.line(frame, xy1, xy2, (0, 0, 255), 5)
+                    
+                    # Obtém a data e hora atual
+                    current_datetime = datetime.datetime.now()
+                    formatted_datetime = current_datetime.strftime("%d/%m/%Y %H:%M:%S")
+                    
+                    # Abre o arquivo CSV e escreve a data e hora
+                    with open('registro_entradas.csv', mode='a', newline='') as file:
+                        writer = csv.writer(file)
+                        writer.writerow([formatted_datetime])
+                        
                     continue
 
                 if c > 0:
                     cv2.line(frame, detect[c - 1], l, (0, 0, 255), 1)
 
-    # Exibindo informações na tela.
+    # Exibe informações sobre o número total de objetos detectados
     cv2.putText(frame, "TOTAL: " + str(total), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
     cv2.putText(frame, "SAINDO: " + str(up), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     cv2.putText(frame, "ENTRANDO: " + str(down), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-    # Exibindo o quadro com as informações.
+    # Exibe a imagem com as informações
     cv2.imshow("frame", frame)
 
-    # Esperando até que a tecla 'q' seja pressionada para encerrar o programa.
+    # Espera até que a tecla 'q' seja pressionada para encerrar o programa
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
 
-# Liberando a captura de vídeo e fechando todas as janelas.
+# Libera a captura de vídeo e fecha todas as janelas
 cap.release()
 cv2.destroyAllWindows()
